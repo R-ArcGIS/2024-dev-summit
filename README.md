d1a76b0eeae51987f00e02981711f78ff118cce8
Josiah Parry <josiah.parry@gmail.com>
2024-03-15 13:05:33 -0700

    start cleaning up

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
 # Esri DevSummit 2024
 
 This repository contains resources used for R-ArcGIS Bridge related presentations and workshops. 
+
+## Whats new in R and ArcGIS
+
+> Solving the world’s complex problems requires bringing together the best available tools, which often means integrating functionality between platforms. The R-ArcGIS Bridge establishes a seamless connection between the vetted spatial analysis powers of ArcGIS and the thousands of cutting-edge libraries for data science and statistical analysis in R. Come see this functionality in action and learn how you can incorporate the newest R-ArcGIS Bridge tools into your workflows.
+
+- [Slides](whats-new-in-r-arcgis/WhatsNew_RandArcGIS_DS2024.pdf)
+- [`{arcgis}` demo script](whats-new-in-r-arcgis/demo.R)
+- [Hierarchical Clustering Geoprocessing Tool using R](whats-new-in-r-arcgis/hclust-gp.R)
+
+## Spatial Data Science in ArcGIS Using Python and R
+
+> Python and R are crucial in data science and provide powerful functionality that expands the capabilities of ArcGIS. This technical session will teach you how to integrate both open-source statistical packages with ArcGIS tools for spatial data science workflows. See how to take the guesswork out of transferring data between ArcGIS and the Python and R environments. The session will expect some knowledge of data manipulation in Python and R, as well as familiarity with running ArcPy geoprocessing tools.
+
+## Contact Us
+
+Please email us at `r_bridge at esri dot com` or [start a discussion post](https://github.com/R-ArcGIS/arcgislayers/discussions).
+
--- whats-new-in-r-arcgis/demo.R
+++ whats-new-in-r-arcgis/demo.R
@@ -1,24 +1,28 @@
-library(arcgis) # https://cran.r-project.org/package=arcgis
+library(arcgis)
 
 
 # Read Services -----------------------------------------------------------
+# Living Atlas USA Major Cities
 # https://esriinc.maps.arcgis.com/home/item.html?id=9df5e769bfe8412b8de36a2e618c7672
 
 # store FeatureLayer url
-furl <-   ""
+furl <- "https://services.arcgis.com/P3ePLMYs2RVChkJx/ArcGIS/rest/services/USA_Major_Cities_/FeatureServer/0"
 
 # create a reference to it
 cities_fl <- arc_open(furl)
 cities_fl
 
+# Read from it:
+arc_select(cities_fl)
 
 # Private Data ------------------------------------------------------------
+# NOTE: this will not work with your machine
+# use a different private layer
 
 # Zip Codes
-# https://analysis-1.maps.arcgis.com/home/item.html?id=bce6b7648a7a4873ba959dce0d3c3f1b
-
 furl <- "https://services1.arcgis.com/hLJbHVT9ZrDIzK0I/arcgis/rest/services/USA_Zip_Code_Locations/FeatureServer/0"
 
+# We expect an error because we haven't authorized
 arc_open(furl)
 
 
@@ -28,16 +32,16 @@ arc_open(furl)
 # Doc: https://r.esri.com/arcgisutils/reference/auth.html
 
 # Different Auth Functions
-code_tkn <- auth_code()   # Interactive and very safe
+code_tkn <- auth_code()     # Interactive and very safe
 client_tkn <- auth_client() # Non-interactive limited
-user_tkn <- auth_user()   # Non-interactive security risk
+user_tkn <- auth_user()     # Non-interactive security risk
 
 
 # Set token to be used
 set_arc_token(code_tkn)
 
 # How do we handle tokens?
-?arc_open()
+# ?arc_open()
 
 # View token
 arc_token()
@@ -57,16 +61,15 @@ flayer
 
 library(sf)
 
+# Add new features to existing services
 to_add <- sf::read_sf("whats-new-in-r-arcgis/data/missing-zip-codes.shp")
-
 add_results <- add_features(flayer, to_add)
 
 
 # Publishing --------------------------------------------------------------
 
-# We take the sf object and publish it
+# Create a new service
 publish_layer(to_add, "Missing Zip Code Points")
-
 
 # Community ---------------------------------------------------------------
 
